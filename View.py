243239701dc0ec83a9f90fdddcff9f83e8999979
Flo <nelles-florian@web.de>
2022-03-20 09:33:15 +0100

    deleted preparations for nice "clean previous lines" feature :v

--- View.py
+++ View.py
@@ -20,7 +20,6 @@ class View:
                               "*  enter 's' during a game to save! *\n"
 
     def load_game(self):
-        self.lastPrintedLines += 1   # answer should be written in same line.
         return ["Do you want to load a previous game? [y/n]: ", self.yes_no, self.true]
 
     @staticmethod
@@ -32,11 +31,10 @@ class View:
                      "")
 
     def AI_opponent(self):
-        self.lastPrintedLines += 1
         return ["Do you want to play against a bot? [y/n]: ", self.yes_no, self.true]
 
-    def wrong_input(self):
-        self.lastPrintedLines += 1
+    @staticmethod
+    def wrong_input():
         return "wrong input, please type a valid answer: "
 
     """OUTDATED"""
@@ -69,32 +67,31 @@ class View:
         self.lastPrintedLines = 8
         return print(self.welcome_string + rows, end="\r")
 
-    def choose_column(self):
-        self.lastPrintedLines += 1
+    @staticmethod
+    def choose_column():
         columns = []
         for i in range(7):
             columns.append(str(i+1))
         columns.append('s')   # for saving the game.
         return ["Select a column: ", columns]
 
-    def print_players_turn(self, player: int, is_AI: bool):
-        self.lastPrintedLines += 1
+    @staticmethod
+    def print_players_turn(player: int, is_AI: bool):
         if is_AI:
             return print("AI " + str(player + 1) + ", it's your turn!")
         return print("Player " + str(player + 1) + ", it's your turn!")
 
-    def print_draw(self):
-        self.lastPrintedLines += 1
+    @staticmethod
+    def print_draw():
         return print("The Game has ended in a draw!")
 
-    def print_winner(self, player: int, is_AI: bool):
-        self.lastPrintedLines += 1
+    @staticmethod
+    def print_winner(player: int, is_AI: bool):
         if is_AI:
             return print("AI " + str(player + 1) + " has won the game!")
         return print("Player " + str(player + 1) + " has won the game!")
 
     def another_game(self):
-        self.lastPrintedLines += 1
         return ["Do you want to play another game? [y/n]: ", self.yes_no, self.true]
 
     @staticmethod
@@ -102,11 +99,10 @@ class View:
         return print("See you next time!")
 
     def confirmation(self):
-        self.lastPrintedLines += 1
         return["Are you sure? [y/n]: ", self.yes_no, self.true]
 
-    def ask_name(self):
-        self.lastPrintedLines += 1
+    @staticmethod
+    def ask_name():
         letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz0123456789"
         return["how would you like to name your SaveFile? (Alphanumeric and Space only)", letters, False]
 
@@ -114,14 +110,11 @@ class View:
         with open('Save_Games/SaveGame_Names.txt', 'r') as file:
             for save in (file.read()).split(';'):
                 self.save_games.append(save)
-                self.lastPrintedLines += 1
                 print(save)
 
     def choose_save_game(self):
-        self.lastPrintedLines += 1
         return["Which of these Save Games would you like to load? ", self.save_games]
 
     def AI_match(self):
-        self.lastPrintedLines += 1
         return["Do you want the AI to play against another AI instead of you? [y/n]: ",
                self.yes_no, self.true]
